e22411f07a77038d90fe55cab2e9a8d7c4c077d4
Stu Elston <stuart.b.elston@gmail.com>
2013-11-15 15:29:10 -0800

    added movement, although not very cleanly...

--- game.py
+++ game.py
@@ -29,10 +29,10 @@ class SceneDrawer(object):
         self.arena = arena
         self.slice_generator = SliceGenerator(arena)
 
-    def draw_scene(self, direction):
+    def draw_scene(self, position, direction):
         for x, pixel_slice in enumerate(
             self.slice_generator.generate_slices(
-                Vector(5, 5),
+                position,
                 direction
             )
         ):
@@ -41,16 +41,52 @@ class SceneDrawer(object):
                     self.pixel_drawer.write_pixel((x, y), pixel)
 
 
+class Dude(object):
+
+    def __init__(self, position=Vector(5,5), angle=0):
+        self.angle = angle
+        self.position = position
+
+    def forward(self):
+        self.position += self.direction * 0.1
+
+    def backward(self):
+        self.position -= self.direction * 0.1
+
+    def rotate_left(self):
+        self._rotate(math.pi/100)
+
+    def rotate_right(self):
+        self._rotate(-math.pi/100)
+
+    def _rotate(self, amount):
+        self.angle += amount
+
+    @property
+    def direction(self):
+        return Vector(math.cos(self.angle), math.sin(self.angle))
+
+
 if __name__ == '__main__':
     screen = pygame.display.set_mode((200, 200))
     clock = pygame.time.Clock()
     pd = PyGamePixelDrawer(screen)
     sd = SceneDrawer(pd, generate_test_scene())
+    dude = Dude()
     running = True
     t = 0
     while running:
         pd.clear()
-        sd.draw_scene(Vector(math.sin(t), math.cos(t)))
+        sd.draw_scene(dude.position, dude.direction)
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_w]:
+            dude.forward()
+        if keys_pressed[pygame.K_s]:
+            dude.backward()
+        if keys_pressed[pygame.K_a]:
+            dude.rotate_left()
+        if keys_pressed[pygame.K_d]:
+            dude.rotate_right()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
